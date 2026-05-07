312f1be5a7f9e34fa6d1995949030a4e285f1349
Guilherme Carneiro <guilhermecar562@gmail.com>
2026-05-06 22:08:12 -0300

    ajuste do tempo de token, de 7 dias e  7 hrs rpa 180 dias

--- carcara/carcara/settings.py
+++ carcara/carcara/settings.py
@@ -119,8 +119,8 @@ REST_FRAMEWORK = {
 
 # ── SimpleJWT ─────────────────────────────────────────────────────────────────
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME":    timedelta(minutes=60),
-    "REFRESH_TOKEN_LIFETIME":   timedelta(days=7),
+    "ACCESS_TOKEN_LIFETIME":    timedelta(days=180),
+    "REFRESH_TOKEN_LIFETIME":   timedelta(days=180),
     "ROTATE_REFRESH_TOKENS":    True,
     "BLACKLIST_AFTER_ROTATION": True,
     "ALGORITHM":    "HS256",
